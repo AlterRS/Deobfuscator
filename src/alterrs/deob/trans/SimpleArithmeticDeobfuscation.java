b8f1ec789afe998c54939613e744f346855d801c
Shawn Davies <shawn@opensource.(none)>
2012-12-16 13:48:06 -0500

    Made the arithmetic deobfuscation more reliable, and added support for negation opcodes.

--- src/alterrs/deob/trans/SimpleArithmeticDeobfuscation.java
+++ src/alterrs/deob/trans/SimpleArithmeticDeobfuscation.java
@@ -30,14 +30,21 @@ import alterrs.deob.util.TreeNodeVisitor;
  */
 public class SimpleArithmeticDeobfuscation extends TreeNodeVisitor {
 
-	private int addLogic;
-	private int subLogic;
-	private int lastSubs = 0;
+	/**
+	 * Counter fields for result printing.
+	 */
+	private int addLogic, subLogic, negationLogic;
+	/**
+	 * The fixable expressions (non-negated). These are stored here until {@code onFinish} is invoked
+	 * because replacing during visitation causes the thread to slow to a lock.
+	 */
 	private final Map<ArithExpr, Boolean[]> fixable = new HashMap<>();
-	
+
+	@Override
 	public void onFinish() {
-		System.out.println("Collected "+fixable.size()+" fixable pieces of arithmetic logic. Iterating...");
+		System.out.println("Collected "+(fixable.size() + negationLogic)+" fixable pieces of arithmetic logic. Iterating...");
 		for (ArithExpr expr : fixable.keySet()) {
+			if (expr.hasParent()) {
 				Boolean[] flags = fixable.get(expr);
 				boolean op_add = flags[0];
 
@@ -49,26 +56,41 @@ public class SimpleArithmeticDeobfuscation extends TreeNodeVisitor {
 
 				boolean left = flags[0];
 				expr.replaceWith(new ArithExpr(op_add ? ArithExpr.SUB : ArithExpr.ADD, left ? expr.right() : expr.left(), left ? expr.left() : expr.right(), expr.type()));
+			}
 		}
-		System.out.println("Arithmetic logic corrected: IADD Logic: "+addLogic+", ISUB Logic: "+subLogic);
+		System.out.println("Arithmetic logic corrected: IADD Logic: "+addLogic+", ISUB Logic: "+subLogic+", Negation logic: "+negationLogic);
 	}
 
 	@Override
 	public void visitArithExpr(final ClassNode c, final MethodNode m, final ArithExpr expr) {
 		try {
+			boolean isNegatedLeft = expr.left() instanceof NegExpr;
+			boolean isNegated = isNegatedLeft || expr.right() instanceof NegExpr;
+			
 			if (expr.hasParent() && expr.left().type().isIntegral() && expr.right().type().isIntegral()) {
 				Number leftVal = getValue(expr.left());
 				Number rightVal = getValue(expr.right());
-				if (leftVal != null && rightVal != null) {
-					boolean isLeft = leftVal.longValue() < 0;
+				
+				if (leftVal != null && rightVal != null || isNegated) {
+					boolean isLeft = isNegated ? isNegatedLeft : leftVal.longValue() < 0;
+					
 					switch(expr.operation()) {
 					case ArithExpr.ADD:
 					case ArithExpr.SUB:
+						if (isNegated) {
+							NegExpr negation = (NegExpr) (isNegatedLeft ? expr.left() : expr.right());
+							
+							expr.replaceWith(new ArithExpr(expr.operation() == ArithExpr.ADD ? ArithExpr.SUB : ArithExpr.ADD, isNegatedLeft ? negation.expr() : expr.left(), isNegatedLeft ? expr.right() : negation.expr(), expr.type()));
+							negationLogic++;
+							break;
+						}
+						
 						if (isLeft || rightVal.longValue() < 0) {
 							fixable.put(expr, new Boolean[] { expr.operation() == ArithExpr.ADD, isLeft });
 						}
 						break;
 					}
+					
 				}
 			}
 		} catch (Exception e) {
@@ -76,18 +98,28 @@ public class SimpleArithmeticDeobfuscation extends TreeNodeVisitor {
 		}
 	}
 
-	public Number add(Expr left, Expr right, boolean get) {
-		Number leftVal = getValue(left);
-		Number rightVal = getValue(right);
-
-		if (leftVal == null || rightVal == null) {
-			return null;
+	/**
+	 * Gets the number value for the specified expression, or null if
+	 * there is no usable number.
+	 * @param expr The expression to get the value of.
+	 * @return The number value.
+	 */
+	public static final Number getValue(Expr expr) {
+		if (expr instanceof LocalExpr) {
+			return ((LocalExpr) expr).index();
 		}
+		if (expr instanceof ArithExpr) {
+			ArithExpr arith = (ArithExpr) expr;
 
-		return leftVal.longValue() + rightVal.longValue();
-	}
-
-	public Number getValue(Expr expr) {
+			Number lv = getValue(arith.left());
+			if (lv != null && lv.longValue() < 0) {
+				return lv;
+			}
+			Number rv = getValue(arith.right());
+			if (rv != null && rv.longValue() < 0) {
+				return rv;
+			}
+		}
 		if (expr instanceof ConstantExpr) {
 			ConstantExpr cst = (ConstantExpr) expr;
 			if (cst.value() instanceof Long) {
@@ -103,21 +135,6 @@ public class SimpleArithmeticDeobfuscation extends TreeNodeVisitor {
 				return (byte) cst.value();
 			}
 			return (int) cst.value();
-		}
-		if (expr instanceof LocalExpr) {
-			return ((LocalExpr) expr).index();
-		}
-		if (expr instanceof NegExpr) {
-			return getValue(((NegExpr) expr).expr());
-		}
-		if (expr instanceof ArithExpr) {
-			ArithExpr arith = (ArithExpr) expr;
-
-			Number lv = getValue(arith.left());
-			if (lv != null) {
-				return lv;
-			}
-			return getValue(arith.right());
 		}
 		return null;
 	}
